0f5eab3ab852b629ee58e0d0946a71838e34c85f
Sung321 <sungugkim@outlook.com>
2020-03-05 12:27:25 -0500

    Uloading Unit file

---
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            for (int i = 2; i < candidate / 2; i++)
+            {
+                if (candidate % 1 == 0)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
